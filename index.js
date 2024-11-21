68113f0fea716921c72087e97bda69a5773d0e1a
Jarbas <jarbasluterooficial@gmail.com>
2024-11-21 11:04:16 -0300

    Update index.js
    Detalhamento das exigências.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+// Variáveis para armazenar o nome e a quantidade de experiência do herói
+let nome = prompt("Digite o nome do herói:");
+let xp = parseInt(prompt("Digite a quantidade de experiência (XP) do herói:"));
+
+// Variável para armazenar o nível do herói
+let nivel = "";
+
+// Estrutura de decisão para determinar o nível do herói com base no XP
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp <= 5000) {
+    nivel = "Prata";
+} else if (xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp <= 8000) {
+    nivel = "Platina";
+} else if (xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Exibição da mensagem final
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
